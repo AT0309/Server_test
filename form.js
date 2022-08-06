6d9129c8668ee85012f68dda7092265e67906e28
AT0309 <84392299+AT0309@users.noreply.github.com>
2022-08-07 00:39:01 +0900

    Update form.js

--- form.js
+++ form.js
@@ -1,20 +1,19 @@
-var prefecture=document.getElementById("prefecture");
 
-console.log("search");
-$.ajax({
-  url: "https://opendata.resas-portal.go.jp/api/v1/prefectures/",
-  type: "GET",
-  headers: {
-      'X-API-KEY':"WP4ecYE1jXwTwoaSk0OnZxzXa6YzDZohjQNWNA9G"
-  },
-  async: "false",
-  success: function(result_data) {
-      for(let i=0; i<result_data.result.length; i++){
-        var a=document.createElement("option");
-        a.innerText=result_data.result[i].prefName;
-        a.value=i+1;
-        prefecture.appendChild(a);
+    var prefecture=document.getElementById("prefecture");
+    console.log("search");
+    $.ajax({
+      url: "https://opendata.resas-portal.go.jp/api/v1/prefectures/",
+      type: "GET",
+      headers: {
+          'X-API-KEY':"WP4ecYE1jXwTwoaSk0OnZxzXa6YzDZohjQNWNA9G"
+      },
+      async: "false",
+      success: function(result_data) {
+          for(let i=0; i<result_data.result.length; i++){
+            var a=document.createElement("option");
+            a.innerText=result_data.result[i].prefName;
+            a.value=i+1;
+            prefecture.appendChild(a);
+          }
       }
-  }
-});
-console.log("API");
+    });
